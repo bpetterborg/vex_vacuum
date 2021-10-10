979edc06d6e0e77362feb8a89a6c234306eb7b5f
bpetterborg <ben.petterborg@gmail.com>
2021-10-10 19:21:27 -0400

    Move everything from pi/ to /.

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#
+#   Vex Robotics Magnet Roomba Thing
+#   
+#	TODO:
+#		- Figure out GPS, mapping
+# 		- Move all config variables to a .json or something
+#		- venv
+#		- Make a webapp to control this (maybe have this one as a the backend)
+#
+#
+
+
+# imports
+import json
+
+import motors
+
