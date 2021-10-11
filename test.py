33a4318ed881a5922b7d14f5d110fb59c4e85c64
bpetterborg <ben.petterborg@gmail.com>
2021-10-10 20:40:20 -0400

    Testing stuff

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 # file where i test everything
 
 import motors
+import time
+
+motors = motors.Motors()
+
+try:
+	motors.Left.spin(20)
+	motors.Right.spin(20)
+	time.sleep(2000)
+	motors.stop()
+	
+except KeyboardInterrupt:
+	print("Stopping")
+	motors.stop()
+	motors.clean_gpio()
