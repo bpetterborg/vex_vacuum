33f4d54e5b8c6ae947828d16ef6ace032d9e56fc
bpetterborg <ben.petterborg@gmail.com>
2021-10-22 08:57:07 -0400

    Add stuff for displaying stuff onscreen, add to TODO

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # 		- Move all config variables to a .json or something
 #		- venv
 #		- Make a webapp to control this (maybe have this one as a the backend)
+#		- Object Detection (sonar, bumpers, cv, etc.)
 #
 #
 
@@ -15,5 +16,14 @@
 # imports
 import json
 
-import motors
+import pi_vex_393, screen
 
+
+
+while True:
+	screen.drawIP()
+	screen.drawSystemLoad()
+	screen.drawDebugLine('PWR ON')
+	screen.drawMessage('WELCOME')
+
+	
