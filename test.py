06ec91431e1f081acfb833c09df477c20ad6ffe3
bpetterborg <ben.petterborg@gmail.com>
2021-10-22 08:57:49 -0400

    Testing screen and motors now

--- test.py
+++ test.py
@@ -1,17 +1,29 @@
 # file where i test everything
 
-import pi_vex_393	# fix this
+import pi_vex_393, screen	# fix this
 import time
 
 motors = pi_vex_393.Motors()
+screen = screen.Screen()
 
 try:
+	screen.drawDebugLine('START')
+	screen.drawMessage('RUNNING')
+
 	motors.Left.spin(20)
 	motors.Right.spin(20)
+	screen.drawDebugLine('SPIN L AND R MTRS')
 	time.sleep(2000)
+
 	motors.stop()
+	screen.drawDebugLine('STOP MTRS')
+	screen.drawMessage('TASK DONE')
 	
 except KeyboardInterrupt:
-	print("Stopping")
+	
+	print('STOPPING')
+	screen.drawMessage('KB INT')
+	screen.drawDebugLine('STOPPING')
+
 	motors.stop()
 	motors.clean_gpio()
